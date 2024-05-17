39d81c3886434f368759d2ffa96099ff9bc424df
mj.kim <alswo9853@gmail.com>
2024-05-17 19:30:26 +0900

    refactor: 카드 데이터 저장

--- src/main/webapp/js/cards.js
+++ src/main/webapp/js/cards.js
@@ -1,15 +1,23 @@
 var card_lists = [];
 
-// 서버로부터 데이터를 가져와서 card_lists 배열에 추가
-fetch('./tarot-cards')
-    .then(response => response.json())
-    .then(data => {
-        card_lists = data.map(card => ({
-            nameEn: card.nameEn,
-            nameKo: card.nameKo,
-            url: card.url,
-            description: card.description
-        }));
-        console.log(card_lists); // 데이터 확인을 위해 출력
-    })
-    .catch(error => console.error('Error fetching tarot cards:', error));
+function fetchTarotCards() {
+    if (card_lists.length === 0) {
+        fetch('./tarot-cards')
+            .then(response => response.json())
+            .then(data => {
+                card_lists = data.map(card => ({
+                    nameEn: card.nameEn,
+                    nameKo: card.nameKo,
+                    url: card.url,
+                    description: card.description
+                }));
+                console.log(card_lists); // 데이터 확인을 위해 출력
+            })
+            .catch(error => console.error('Error fetching tarot cards:', error));
+    } else {
+        console.log('Tarot cards already fetched.');
+    }
+}
+
+// 함수 호출
+fetchTarotCards();
